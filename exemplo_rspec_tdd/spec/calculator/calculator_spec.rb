eab90f92ba2de2e0449bacdb57b54e380df396ff
Felbio <felipe.oliveira@walljobs.com.br>
2024-09-17 15:38:41 -0300

    add comments

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -7,9 +7,16 @@ describe Calculator do
     expect(result).to eq(12)  
   end
 
-  it 'use sum method for 2 numbers with negative numbers' do
+  specify 'use sum method for 2 numbers with negative numbers' do
     calc = Calculator.new
     result = calc.sum(-5,7)
     expect(result).to eq(2)  
   end
 end
+# it specify or example
+# to run you can use also: rspec spec/meuteste_spec.rb
+# rspec spec/calculator/calculator_spec.rb
+# Fases do teste
+# preparar, interagir e verificar 
+# (e o teardown - quando voce coloca o sistema no estado
+#  em que ele estava antes do teste ser executado)
